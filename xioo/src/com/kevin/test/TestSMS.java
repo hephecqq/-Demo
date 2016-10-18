098f0e88cbc31305e0f173277349de34b8c038e1
DAGONG <hephec@126.com>
2016-10-18 11:46:25 +0800

    test

---
+++ xioo/src/com/kevin/test/TestSMS.java
@@ -0,0 +1,123 @@
+package com.kevin.test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import org.junit.Test;
+
+public class TestSMS {
+	/**
+	 * @Param name
+	 * @param pwd
+	 * @param mobile
+	 * @param content
+	 * @param stime
+	 * @param sign
+	 * @param type
+	 * @param extno
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test() throws Exception{
+		
+		StringBuffer param = new StringBuffer();
+    	param.append("name="+"hephec");
+    	param.append("&pwd="+"251C7B8D201C6F533177B45BBC23");
+    	param.append("&mobile=").append("17010282708");
+    	param.append("&content=").append(URLEncoder.encode("1234".toString(),"UTF-8"));
+    	param.append("&stime=");
+    	param.append("&sign=").append(URLEncoder.encode("hephec","UTF-8"));
+    	param.append("&type=pt");
+    	param.append("&extno=").append("");
+    	
+		URL localURL = new URL("http://web.cr6868.com/asmx/smsservice.aspx?");
+        URLConnection connection = localURL.openConnection();
+        HttpURLConnection httpURLConnection = (HttpURLConnection)connection;
+        
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Accept-Charset", "utf-8");
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        httpURLConnection.setRequestProperty("Content-Length", String.valueOf(param.length()));
+        
+        OutputStream outputStream = null;
+        OutputStreamWriter outputStreamWriter = null;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+        String resultBuffer = "";
+        try {
+            outputStream = httpURLConnection.getOutputStream();
+            outputStreamWriter = new OutputStreamWriter(outputStream);
+            
+            outputStreamWriter.write(param.toString());
+            outputStreamWriter.flush();
+            
+            if (httpURLConnection.getResponseCode() >= 300) {
+                throw new Exception("HTTP Request is not success, Response code is " + httpURLConnection.getResponseCode());
+            }
+            
+            inputStream = httpURLConnection.getInputStream();
+            resultBuffer = convertStreamToString(inputStream);
+            System.out.println(resultBuffer);
+            
+        } finally {
+            
+            if (outputStreamWriter != null) {
+                outputStreamWriter.close();
+            }
+            
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            
+            if (reader != null) {
+                reader.close();
+            }
+            
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+            
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            
+        }
+	
+    }
+	/**
+	 * 转换返回值类型为UTF-8格式.
+	 * @param is
+	 * @return
+	 */
+	public static String convertStreamToString(InputStream is) {    
+        StringBuilder sb1 = new StringBuilder();    
+        byte[] bytes = new byte[4096];  
+        int size = 0;  
+        
+        try {    
+        	while ((size = is.read(bytes)) > 0) {  
+                String str = new String(bytes, 0, size, "UTF-8");  
+                sb1.append(str);  
+            }  
+        } catch (IOException e) {    
+            e.printStackTrace();    
+        } finally {    
+            try {    
+                is.close();    
+            } catch (IOException e) {    
+               e.printStackTrace();    
+            }    
+        }    
+        return sb1.toString();    
+    }
+}
